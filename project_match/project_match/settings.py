029aefd9f1b05f7a8e7a2485a9e4c289d3c5bcef
Xiwen Zhang <xiwen.zhang@pwc.com>
2022-05-23 22:04:19 -0400

    security change

--- project_match/project_match/settings.py
+++ project_match/project_match/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w3^6kmqua3s*=@g39gs+7mc+b#g$qxh%q+wi)eove1^wu%e)r%'
+# SECRET_KEY = 'django-insecure-w3^6kmqua3s*=@g39gs+7mc+b#g$qxh%q+wi)eove1^wu%e)r%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
